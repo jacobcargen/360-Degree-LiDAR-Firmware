0603ddec6116d094e43305a4c9b9f5716a3ea9b0
obrignati <obrignati@gmail.com>
2026-02-18 11:13:52 -0500

    looks good

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
-export PATH=$PATH:$HOME/bin
-arduino-cli compile --fqbn esp32:esp32:esp32c3 firmware/
+export PATH="$PATH:$HOME/bin"
+
+FQBN="esp32:esp32:XIAO_ESP32C3"
+
+arduino-cli compile --fqbn "$FQBN" firmware/
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,8 +1,10 @@
 void setup() {
   Serial.begin(115200);
+  while (!Serial) { delay(10); }
   Serial.println("Hello World ESP32-C3");
 }
 
 void loop() {
   delay(1000);
+  Serial.println("Hello World ESP32-C3");
 }
