0e49451ca9e465dd16b52337a060f86aa6ec5a3b
obrignati <obrignati@gmail.com>
2026-03-04 15:09:46 -0500

    updates

--- upload_mac.sh
+++ upload_mac.sh
@@ -2,4 +2,4 @@
 set -e
 
 export PATH=$PATH:$HOME/bin
-arduino-cli upload --fqbn esp32:esp32:esp32c3 --port ${1:-/dev/cu.usbmodem1101} firmware/
+arduino-cli upload --fqbn esp32:esp32:esp32c3 --port ${1:-/dev/uv.usbmodem1101} firmware/
