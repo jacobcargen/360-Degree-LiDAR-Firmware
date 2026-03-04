ab1c6495deee9c5cca72cffb3abcb11cf8227670
obrignati <obrignati@gmail.com>
2026-03-04 12:29:59 -0500

    Added lots of wifi code

--- firmware/WiFiManager.cpp
+++ firmware/WiFiManager.cpp
@@ -1,1 +1,24 @@
 #include "WiFiManager.h"
+
+WiFiManager::WiFiManager(const char* ssid, const char* password)
+    : _ssid(ssid), _password(password) {}
+
+void WiFiManager::begin() {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(_ssid, _password);
+
+    Serial.print("Connecting to WiFi");
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.print("\nConnected :)");
+    Serial.print("IP address:  ");
+    Serial.println(WiFi.localIP());
+}
+
+bool WiFiManager::isConnected() {
+    return WiFi.status() == WL_CONNECTED;
+}
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,5 +1,10 @@
+#include "WiFiManager.h"
+
+WiFiManager wifi("SpectrumSetup-7F", "lightwater958");
+
 void setup() {
   Serial.begin(115200);
+  WiFi.begin();
   Serial.println("Hello World ESP32-C3");
 }
 
